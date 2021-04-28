6dbf6d12cbf7fc14821e1a7d9b4b659f0e57ac1e
jackjack3006 <jvsanoj23@gmail.com>
2021-04-28 09:19:20 +0530

    first class coding

---
+++ Unit 1/program_structure.c
@@ -0,0 +1,10 @@
+// Program Structure
+// It is case sensitive
+// C is free format source code =  That is indentation doesn't matter
+
+# include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+}
