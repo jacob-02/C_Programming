e092a8d1cf11f6cc35070ffeac7293f6f365c462
jackjack3006 <jvsanoj23@gmail.com>
2021-04-28 09:28:52 +0530

    Creating functinos

--- Unit 1/program_structure.c
+++ Unit 1/program_structure.c
@@ -2,7 +2,7 @@
 // It is case sensitive
 // C is free format source code =  That is indentation doesn't matter
 
-# include <stdio.h>
+# include <stdio.h> // Pre processor directive 
 
 int main()
 {
