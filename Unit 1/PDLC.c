c6e6a47cf449158e3e94402048f0507f64603821
jackjack3006 <jvsanoj23@gmail.com>
2021-04-28 09:38:51 +0530

    PDLC

---
+++ Unit 1/PDLC.c
@@ -0,0 +1,27 @@
+// Program Development Life Cycle
+
+/*
+    Editing
+    Preprocessing
+    Complilation
+    Linking
+    Loading
+    Execution
+
+Editing:
+    Writing a program
+    Save the file : file_name.c
+
+Preprocessing:
+    Macro Substitution - #define stuff and all that 
+    Comments Removed
+    Expansion of the files
+
+Output:
+    Intermediate file
+    Assembly level file
+    These 2 together are called as Translation Unit
+
+
+
+*/
