5057e4397ca335b241a5c04ae349266b6ba74aae
jackjack3006 <jvsanoj23@gmail.com>
2021-04-29 10:21:21 +0530

    ErrorsLearning

---
+++ Unit 1/Errors_In_C.c
@@ -0,0 +1,13 @@
+/*
+
+Errors in C
+    Compile Time Errors
+    Link Time Errors
+    Run Time Errors
+    Logical Errors
+
+Compile Time Errors
+    Object File doesn't get created in Compile time errors
+    
+
+*/
