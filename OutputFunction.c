205c8914deb5c85b9221e0ae2f86cd052f039cdc
Jacob <jvsanoj23@gmail.com>
2021-05-03 08:32:36 +0530

    New Changes

---
+++ OutputFunction.c
@@ -0,0 +1,18 @@
+/*
+Input Output Functions
+    formatted
+    unformatted
+
+printf => formatted output function
+    defined <stdio.h>
+    int printf(const char *format, ...);
+    variable number of arguments
+    format string
+    "%format specifier [flags][width][precisiion]"
+
+%c => char
+%d => integer
+%f => float
+%lf => double
+
+*/
