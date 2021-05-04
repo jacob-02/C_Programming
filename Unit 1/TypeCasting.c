588ecf29d601c3d866550ba5e3c876ec754bf271
Jacob <jvsanoj23@gmail.com>
2021-05-04 10:36:38 +0530

    Type Casting

---
+++ Unit 1/TypeCasting.c
@@ -0,0 +1,14 @@
+/*
+
+Type Casting
+
+    Implicit 
+        This is done by the compiler
+        
+        Example:
+
+            float a = 2.5;
+            int sum = (int)(a) + 4;
+            printf("%d\n", sum);
+
+*/
