71eeb47b176d18a21bf6ee8e603d77589e58e09a
Jacob <jvsanoj23@gmail.com>
2021-05-04 10:29:28 +0530

    Operators

---
+++ operators.c
@@ -0,0 +1,17 @@
+/*
+
+Operators
+    types of operators
+    based on the operands 
+        unary - one operator
+        binary - 2 operators
+        ternary - 3 operators
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%d %f %f %f", 25/4, 25.0/4.0, 25.0/4, 25/4.0);
+}
