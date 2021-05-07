5a0590f30302a8b640d4b5eeda584e00a19eda84
jackjack3006 <jvsanoj23@gmail.com>
2021-05-07 08:30:34 +0530

    Using while loops

---
+++ Unit 1/ControlStructures.c
@@ -0,0 +1,26 @@
+/*
+Control Structures:
+    
+    Looping structures:
+        -while
+        -for    
+        -do while
+    
+    Selection Structures:
+        -for
+        -else if
+        -else
+        -nested if
+        -switch
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+    while (--n)
+    {
+        printf("%d", n);
+    }
+}
