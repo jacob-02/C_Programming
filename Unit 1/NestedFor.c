c658651f6f7edc9b119391be3f123ddbff3e0dfc
jackjack3006 <jvsanoj23@gmail.com>
2021-05-08 09:15:50 +0530

    Using nested loops

--- Unit 1/NestedFor.c
+++ Unit 1/NestedFor.c
@@ -11,20 +11,33 @@ int main()
         printf("\n");        
     }
 
-    int i,j;
+    int i,j, sum = 0;
     i = 1;
+
+    while (i <= n)
+    {
+        j = 1, sum = 0;
+        while (j <= i)
+        {
+            printf("%d ", j);
+            sum += j;
+            j++;
+        }
+        i++;
+        printf(" = %d", sum);
+        printf("\n");
+    }
 
     while (i <= n)
     {
         j = 1;
         while (j <= i)
         {
-            printf("%d ", j);
+            printf("%c ", 'A'+j-1);     // We can get the ASCII directly using %c and 'Alphabet'
             j++;
         }
         i++;
         printf("\n");
-    }
-    
+    }  
 
 }
