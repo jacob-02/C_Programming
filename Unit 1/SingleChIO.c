96bcb271b184edcff140929b5cfa5575666a747b
jackjack3006 <jvsanoj23@gmail.com>
2021-05-10 09:05:39 +0530

    Single Character I/O

--- Unit 1/SingleChIO.c
+++ Unit 1/SingleChIO.c
@@ -1,10 +1,10 @@
 //In this we can take input from any place other than the keyboard or the terminal
 
-#include <stdio.h>
+//#include <stdio.h>
 
 int main()
 {
     char ch;
-    ch = getc(stdin);
-    putc(ch, stdout);
+    ch = getc();
+    putc(ch);
 }
