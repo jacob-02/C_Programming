51bd3f4b3fe9690257c6f9154bef527e3aa4aa2a
jackjack3006 <jvsanoj23@gmail.com>
2021-05-12 09:36:07 +0530

    Using functions

--- Unit 1/MoreFunctions.c
+++ Unit 1/MoreFunctions.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
- void display(int a, int b)
+ int display(int a, int b)
  {
      printf("Hello world\n");
      printf("%d %d\n", a, b);
+     return a+b;
  }
 
  int main()
  {
      int a, b;
      a=10, b=20;
-     display(a, b);
+     int sum=display(a, b);
+     printf("%d\n", sum);
  }
+
+ /*
+ An activation record is created whenever a function call is made
+ Stack frame
+    Variables in the function : Local Variable of Callee
+    Return address of the caller
+    Location to store the return value
+    Parameters in the the callee
+    Temporary variables
+ */
