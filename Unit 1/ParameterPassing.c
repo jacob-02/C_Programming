56c8d35c12466e5dd1242688639d5e9c08a9e4e3
jackjack3006 <jvsanoj23@gmail.com>
2021-05-13 08:51:18 +0530

    Passing parameters

--- Unit 1/ParameterPassing.c
+++ Unit 1/ParameterPassing.c
@@ -9,19 +9,27 @@ Funciton Definition - Parameters
 */
 
 #include <stdio.h>
-
-void display(int a)
+void display(int* a);
+/*
+void display(int* a)
 {
     printf("before a=%d\n", a);
-    a = 200;
-    printf("after passing a=%d\n", a); 
+    *a = 200;
+    printf("after passing a=%d\n", *a); 
 }
-
+*/
+void display(int* a)
+{
+    int b=200;
+    printf("before a=%d\n", *a);
+    *a=b;       // *a is used to change the value of the original value. This is called the dereferencing operator
+    printf("after passing a=%d\n", *a); 
+}
 int main()
 {
     int a = 100;
     printf("before passing a=%d\n", a);
-    display(a);
+    display(&a);
     printf("after passing a=%d\n", a);
     return 0;
 }
