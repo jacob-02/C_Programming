59432b6c79a11786716f9c0dab33d8a3ed7e8040
jackjack3006 <jvsanoj23@gmail.com>
2021-05-17 09:02:25 +0530

    Recursion

--- Unit 1/recursion.c
+++ Unit 1/recursion.c
@@ -3,9 +3,17 @@ A function calling itself. The same crap as in python
 */
 
 #include <stdio.h>
+int factorial(int n);
 
 int main()
 {
-    printf("hello world\n");
-    main();
+    int n = 3;
+    int fact = factorial(n);
+    printf("Factorial of %d is %d\n", n, fact);
+}
+
+int factorial(int n)
+{
+    return n = n*(n-1);
+    factorial;
 }
