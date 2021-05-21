7e57544774f5036a6f3c9e21c436a81180c88433
jackjack3006 <jvsanoj23@gmail.com>
2021-05-21 09:00:27 +0530

    Using arrays with loops and other thign

--- Unit 1/moreArray.c
+++ Unit 1/moreArray.c
@@ -8,10 +8,10 @@ int main()
     scanf("%d", &n);
     printf("Enter the elements of the array\n");
 
-    for(int i=0; i<=(n-1); i++)
+    for(int i=0; i<(n-1); i++)
         scanf("%d ", &a[i]);
 
-    for(int i=0; i<n; i++)
+    for(int i=0; i<(n-1); i++)
         printf("%d ", a[i]);
 
     int sum = 0;
@@ -20,4 +20,24 @@ int main()
 
     printf("\nSum of the elements in the array: %d\n", sum);   
     printf("The average of the values is equal to : %d\n", sum/n); 
+
+    int b[100];
+    n = 20;
+
+    for(int i=0; i<(n-1); i++)
+    {
+        if(i==0)
+            b[i] = 0;
+        else if(i == 1)
+            b[i] = 1;
+        else
+        {
+            b[i] = b[i-1] + b[i-2];
+        }
+    }
+
+    for(int i=0; i<(n-1); i++)
+        printf("%d ", b[i]); 
+    
+    printf("\n");
 }
