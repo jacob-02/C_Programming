ceb7af6f5e2676bbbe142e03701da3a3999a3afe
jackjack3006 <jvsanoj23@gmail.com>
2021-05-24 08:36:05 +0530

    Array traversal using an array

--- Unit 1/pointer.c
+++ Unit 1/pointer.c
@@ -12,5 +12,16 @@ int main()
 
     printf("p+1 = %p\t", (int *)p+1);   // Type casts p+1 to an integer
     printf("p+1 = %p\t", (char *)p+1);   // Type casts p+1 to an char
-    printf("p+1 = %p\t", (double *)p+1);   // Type casts p+1 to an double
+    printf("p+1 = %p\n\n", (double *)p+1);   // Type casts p+1 to an double
+
+    // Array traversal using pointers
+
+    int arr[] = {12, 33, 44};
+    int *p2 = arr;  // No ampersand for array is already an address of the first value in the array
+    printf("Before increment %p %d\n", p2, *p2);   // Prints the value of the first value in the array
+    p2++;
+    printf("After increment %p %d\n", p2, *p2);    // Prints the value of the second value of the array because of the increment of the address
+
+    // This shows that the array elements are arranged one after another in the memory
+
 }
