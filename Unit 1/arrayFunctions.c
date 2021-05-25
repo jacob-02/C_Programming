83d19672c9041d7da50decd24caac484cd737695
jackjack3006 <jvsanoj23@gmail.com>
2021-05-25 10:39:30 +0530

    Using array with functions

--- Unit 1/arrayFunctions.c
+++ Unit 1/arrayFunctions.c
@@ -2,13 +2,14 @@
 
 void f1(int a[])
 {
-
+    int n = sizeof(a)/sizeof(a[0]);
+    printf("Array size inside the f1 function is %d\n", n);
 }
 
 int main()
 {
     int a[] = {1, 2, 3, 4, 5};
     int n = sizeof(a)/sizeof(a[0]);
-    printf("Array size inside the main function is%d\n", n);
+    printf("Array size inside the main function is %d\n", n);
     f1(a);
 }
