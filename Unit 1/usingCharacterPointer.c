8b0d144361609889f9dc8476a9d14cdd44a571f0
jackjack3006 <jvsanoj23@gmail.com>
2021-05-28 08:30:15 +0530

    How to change the values in an array

--- Unit 1/usingCharacterPointer.c
+++ Unit 1/usingCharacterPointer.c
@@ -2,13 +2,25 @@
 
 int main()
 {
-    char*p = "PESU";
+    // char*p = "PESU";
+    // printf("%s\n", p);
+    // p++;
+    // printf("%s", p);
+    // p--;
+    // printf("%s\n", p);
+    // p[1] = 'e';
+    // printf("%s", p);
+    // printf("Size of the string is %d", sizeof(p));
+
+    // This code is to show that once a value is assigned to an array using the double quotes u can't change using the p[1] = x condition
+    char p[] = "PESU";
+    char p1[] = {'P', 'E', 'S', 'U'};
+    printf("Size of the string 1 is %ld\n", sizeof(p));
+    printf("Size of the string 2 is %ld\n", sizeof(p1));
     printf("%s\n", p);
-    p++;
-    printf("%s", p);
-    p--;
-    printf("%s\n", p);
+    printf("%s\n", p1);
     p[1] = 'e';
-    printf("%s", p);
-    printf("Size of the string is %d", sizeof(p));
+    p1[1] = 'e';
+    printf("%s\n", p);
+    printf("%s\n", p1);
 }
