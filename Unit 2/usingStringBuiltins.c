2c1d8a0b4646eea2de9e565b1be2d2e9e4ab9bd0
jackjack3006 <jvsanoj23@gmail.com>
2021-05-28 08:42:34 +0530

    Using string functions

--- Unit 2/usingStringBuiltins.c
+++ Unit 2/usingStringBuiltins.c
@@ -3,7 +3,19 @@
 
 int main()
 {
-    char a[] = {'P', 'E', 'S', 'U', '\0'};
-    printf("Size of the array is = %d\n", sizeof(a));
-    printf("The length of the string is = %d\n", strlen(a));    // Doens't count the null character
+    // char a[] = {'P', 'E', 'S', 'U', '\0'};
+    // printf("Size of the array is = %d\n", sizeof(a));
+    // printf("The length of the string is = %d\n", strlen(a));    // Doens't count the null character
+
+    char a[] = "PES";
+    char b[10];
+    // a = b;   This is an error
+
+    printf("String a  = %s\n", a);
+    printf("String b  = %s\n", b);
+
+    strcpy(b, a);
+    
+    printf("String a  = %s\n", a);
+    printf("String b  = %s\n", b);
 }
