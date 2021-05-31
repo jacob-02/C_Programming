2e9e98e9e6c896587838e41c369b5ad351945386
jackjack3006 <jvsanoj23@gmail.com>
2021-05-31 08:55:52 +0530

    Making one's own string copy

--- Unit 2/myStrcpyfn.c
+++ Unit 2/myStrcpyfn.c
@@ -10,13 +10,25 @@ int main()
     printf("%s\n", b);
 }
 
-void stringcopy(char b[], char a[])
+// void stringcopy(char b[], char a[])
+// {
+//     int i = 0;
+//     while(a[i] != '\0')
+//     {
+//         b[i] = a[i];
+//         i++;
+//     }
+//     b[i] = '\0';
+// }
+
+void stringcopy(char *b, char *a)
 {
     int i = 0;
-    while(a[i] != '\0')
+    while (*(a) != '\0')
     {
-        b[i] = a[i];
-        i++;
+        *b = *a;
+        b++;
+        a++;
     }
-    b[i] = '\0';
+    *(b + i) = '\0';
 }
