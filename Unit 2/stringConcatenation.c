b63e6d5e42249abc96197511f9af4b274ec6d071
jackjack3006 <jvsanoj23@gmail.com>
2021-06-01 10:25:08 +0530

    Creating a string concatenation function

---
+++ Unit 2/stringConcatenation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void concatenate(char word[], char post[]);
+
+int main()
+{
+    char word[] = "Hello", post[] = "Jacob";
+    concatenate(word, post);
+}
+
+void concatenate(char word[], char post[])
+{
+    int i = 0, j = 0;
+
+    while(word[i] != '\0')
+        ++i;
+    
+    while (post[j] != '\0')
+    {
+        word[i] = post[j];
+        i++;
+        j++;
+    }
+    
+    word[i] = '\0';
+
+    printf("The concatenated word is %s\nLength of the combined word is %ld\n", word, strlen(word));    
+}
