fb69eb96e8c8d4d396712451c505d232851b57b7
jackjack3006 <jvsanoj23@gmail.com>
2021-06-01 14:44:44 +0530

    practice

--- Unit 2/Practice1.c
+++ Unit 2/Practice1.c
@@ -18,7 +18,14 @@ int main()
 
 void character(char arr[], char ch)
 {
+    int count = 0;
     for(int i = 0; i < strlen(arr); i++)
         if(arr[i] == ch) 
+        {
+            count++;
             printf("The character %c is present at %d in the string %s\n", ch, i, arr);
+        }
+    
+    if(count  == 0)
+        printf("The character is not present in the string\n");
 }
