34ba53469924e00e6daa9f5a342417aa3432e264
jackjack3006 <jvsanoj23@gmail.com>
2021-06-02 09:46:09 +0530

    New

--- Unit 2/GDB.c
+++ Unit 2/GDB.c
@@ -15,7 +15,7 @@ int main()
 int factors(int number)
 {
     int sum = 0;
-    for(int i = 0; i < number; i++)
+    for(int i = 1; i <= number; i++)
         if(number % i == 0)
             sum += i;
     
