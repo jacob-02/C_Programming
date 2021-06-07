16a8a0e4975909051eb7120ec00d67146d62df3c
jackjack3006 <jvsanoj23@gmail.com>
2021-06-07 08:46:44 +0530

    Partial Initialisation

--- Unit 3/structures.c
+++ Unit 3/structures.c
@@ -10,8 +10,26 @@ struct Student
 
 int main()
 {
-    struct Student s;
+    struct Student s = {"abc", 43, 78};
     s.marks = 200.9;
     
     printf("%f\n", s.marks);
+    printf("%d\n", s.roll_no);
+    printf("%s\n", s.name);
+
+    printf("New line\n");
+
+    struct Student s1 = {10, "abc"};
+
+    printf("%f\n", s1.marks);
+    printf("%d\n", s1.roll_no);
+    printf("%s\n", s1.name);
+
+    printf("New line\n");
+    
+    struct Student s3 = {name:"Hello", roll_no:7,};
+
+    printf("%f\n", s3.marks);
+    printf("%d\n", s3.roll_no);
+    printf("%s\n", s3.name);
 }
