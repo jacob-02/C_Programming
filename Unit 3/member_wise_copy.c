42a6c8744c816ab79325fff06e9f35519b10b62a
jackjack3006 <jvsanoj23@gmail.com>
2021-06-08 10:23:38 +0530

    Member copy

--- Unit 3/member_wise_copy.c
+++ Unit 3/member_wise_copy.c
@@ -10,5 +10,8 @@ int main()
 {
     struct student s1 = {44, "jacob"};
     struct student s2 = s1;
-    printf("%d %s\n", s2.roll_no, s2.name);
+    printf("%d %s ", s2.roll_no, s2.name);
+    printf("%p %p\n", &s2.roll_no, &s2.name);   // This is to show that the copy acquires a new memory and doesn't refer to the location of the variable from where it was copied
+    printf("%d %s ", s1.roll_no, s1.name);
+    printf("%p %p\n", &s1.roll_no, &s1.name);
 }
