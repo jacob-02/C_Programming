8b6bee690b74b5c0b35bc28f3dd96e9c5fa99c18
jackjack3006 <jvsanoj23@gmail.com>
2021-06-09 09:27:40 +0530

    Inherited structures

---
+++ Unit 3/typdef.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct dob
+{
+    int date, month, year;
+};
+
+struct student
+{
+    int roll_no;
+    char name[20];
+    int marks;
+    struct dob d1;
+};
+
+int main()
+{
+    struct student s1 = {24, "John", 78, {20, 3, 2000}};
+    printf("Roll no %d\n", s1.roll_no);
+    printf("Name = %s\n", s1.name);
+    printf("Marks = %d\n", s1.marks);
+    printf("Date = %d/%d/%d\n", s1.d1.date, s1.d1.month, s1.d1.year);
+}
