6dd06d69d7b67fce9d887522d8431b2ad2cdec7c
jackjack3006 <jvsanoj23@gmail.com>
2021-06-11 08:45:54 +0530

    Use NULL to keep the memory safe

--- Unit 3/dmaErrors.c
+++ Unit 3/dmaErrors.c
@@ -14,9 +14,25 @@ int main()
     printf("%p\n", p1);
     printf("%d\n", *p1);
 
+    printf("%p\n", p);
+    printf("%d\n", *p);
+
     free(p1);
 
     printf("After deallocating the memory\n");
     printf("%p\n", p1);
     printf("%d\n", *p1);
+
+    printf("%p\n", p);
+    printf("%d\n", *p);
+
+    p = NULL;
+    p1 = NULL;
+
+    printf("After NULLing the memory\n");
+    printf("%p\n", p1);
+    printf("%d\n", *p1);
+
+    printf("%p\n", p);
+    printf("%d\n", *p);
 }
