ff33b6a3c3d5368e9485673464a13280e3ef39a5
jackjack3006 <jvsanoj23@gmail.com>
2021-06-11 09:04:59 +0530

    DON'T USE THIS PROGRAM

---
+++ Unit 3/errorsWithRealloc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *p = (int *)calloc(5, sizeof(int));
+
+    printf("Initial address is %p\n", p);
+    printf("Enter the elements\n");
+
+    for (int i = 0; i < 5; i++)
+        scanf("%d", (p + i));
+
+    printf("Final address is %p\n", p);
+    printf("The elements are\n");
+
+    for (int i = 0; i < 5; i++)
+        printf("%d\n", *(p + i));
+
+    int *p2 = (int *)realloc(p, 10 * sizeof(int));
+
+    printf("Initial address is %p\n", p2);
+    printf("Enter the elements\n");
+
+    for (int i = 0; i < 5; i++)
+        scanf("%d", (p2 + i));
+
+    printf("Final address is %p\n", p2);
+    printf("The elements are\n");
+
+    for (int i = 0; i < 5; i++)
+        printf("%d\n", *(p2 + i));
+    
+    free(p);
+    free(p);
+}
