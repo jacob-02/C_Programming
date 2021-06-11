4d4859af2cff5eeeac54556244cb07aabde8e877
jackjack3006 <jvsanoj23@gmail.com>
2021-06-11 08:22:33 +0530

    Using malloc and realloc

--- Unit 3/realloc.c
+++ Unit 3/realloc.c
@@ -3,5 +3,25 @@
 
 int main()
 {
-    
+    int *p = (int *)malloc(3 * sizeof(int));
+
+    if(p == NULL)
+        printf("The memory is allocated\n");
+
+    else
+    {
+        printf("Initial address is %p\n", p);
+        printf("Enter the elements\n");
+
+        for(int i = 0; i < 3; i++)
+            scanf("%d", (p + i));
+        
+        printf("The elements are\n");
+
+        for (int i = 0; i < 3; i++)
+            printf("%d\n", (p + i));
+        
+    }
+
+    p = (int *)realloc(p, 10 * sizeof(int));   
 }
