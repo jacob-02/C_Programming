f789bd0d59cbfd2a2aad90da1387660953367f8a
jackjack3006 <jvsanoj23@gmail.com>
2021-06-15 13:39:05 +0530

    Using calloc to dynamically allocate memory

--- Unit 3/calloc.c
+++ Unit 3/calloc.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void structure();
+void normal();
+
+struct emp
+{
+    int no;
+    char name[20];
+    float salary;
+};
+
+struct emp *ptr;
+
+void structure()
+{
+    printf("Enter the number of elements in the array\n");
+    int n;
+    scanf("%d", &n);
+
+    ptr = (struct emp *)calloc(n, sizeof(struct emp));
+
+    if (ptr == NULL)
+        printf("There is no memory left for allocation\n");
+
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            printf("Enter the employee details\n");
+            scanf("%d%s%f", &(ptr+i)->no, (ptr+i)->name, &(ptr+i)->salary);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+
+            printf("The employee details\n");
+            printf("%d\n%s\n%f\n", (ptr+i)->no, (ptr+i)->name, (ptr+i)->salary);
+        }
+    }
+}
+
+void normal()
 {
     int *x, n, i;
     printf("Enter the values of n\n");
@@ -9,19 +49,19 @@ int main()
 
     x = (int *)calloc(n, sizeof(int));
 
-    if(x == NULL)
+    if (x == NULL)
         printf("Memory is not allocated\n");
 
-    else    
+    else
     {
         printf("Memory is succesfully allocated\n");
         for (int i = 0; i < n; i++)
         {
             printf("%d", *(x + i));
-        }  
+        }
 
         printf("\n");
-        
+
         for (int i = 0; i < n; i++)
         {
             scanf("%d", &x[i]);
@@ -31,5 +71,10 @@ int main()
         {
             printf("%d", *(x + i));
         }
-    }        
+    }
+}
+
+int main()
+{
+    structure();
 }
