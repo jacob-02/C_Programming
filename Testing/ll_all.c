5443d68a87da09ffd01287658ef4ac4ef6d87e67
jackjack3006 <jvsanoj23@gmail.com>
2021-06-19 21:11:02 +0530

    Doesn't work but ok

---
+++ Testing/ll_all.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *link;
+} node_t;
+
+node_t *head;
+
+void display(node_t *p)
+{
+    while (p != NULL)
+    {
+        printf("The element is %d\n", p->data);
+        p = p->link;
+    }
+}
+void insert(int element)
+{
+    node_t *temp = (node_t *)malloc(sizeof(node_t));
+    temp->data = element;
+    temp->link = NULL;
+
+    if (head == NULL || element < head->data)
+    {
+        temp->link = head;
+        head = temp;
+    }
+
+    else
+    {
+        node_t *present = head;
+        node_t *previous = NULL;
+
+        while (present != NULL && present->data < element)
+        {
+            previous = present;
+            present = present->link;
+        }
+        temp->link = present;
+        previous->link = temp;
+    }
+}
+
+int main()
+{
+    head = NULL;
+    int n, element;
+    printf("Enter the number of nodes that you want to enter\n");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element\n");
+        scanf("%d", &n);
+        insert(element);
+        display(head);
+    }
+}
