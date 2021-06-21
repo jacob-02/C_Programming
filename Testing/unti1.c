4affc35c8a1dd77b70fb0374b86283cfcb61a82b
jackjack3006 <jvsanoj23@gmail.com>
2021-06-21 19:29:37 +0530

    Pointers with 2 d arrays

--- Testing/unti1.c
+++ Testing/unti1.c
@@ -8,10 +8,17 @@ void swap(int *a, int *b)
 }
 int main()
 {
-    int a = 100;
-    int b = 200;
-    printf("before call a is %d and b is %d\n", a, b);
-    // a is 100 and b is
-    swap(&a, &b);
-    printf("after call a is %d and b is %d\n", a, b);
+    // int a = 100;
+    // int b = 200;
+    // printf("before call a is %d and b is %d\n", a, b);
+    // // a is 100 and b is
+    // swap(&a, &b);
+    // printf("after call a is %d and b is %d\n", a, b);
+
+    int arr[4][3] = {33, 44, 11, 55, 88, 22, 33, 66, 99, 11, 80, 9};
+    int(*p)[3] = arr;
+    printf("%d\n", p[2][1]);
+    printf("%d\n", *(*(p + 2) + 1));
+    printf("%d\n", sizeof(p));
+    printf("%d %d\n", sizeof(*p), sizeof(arr));
 }
