46fb58fea61098a3e220a4132bdc0aa7ddeb949d
jackjack3006 <jvsanoj23@gmail.com>
2021-06-21 19:58:07 +0530

    Using typedef

--- Testing/unti1.c
+++ Testing/unti1.c
@@ -15,10 +15,14 @@ int main()
     // swap(&a, &b);
     // printf("after call a is %d and b is %d\n", a, b);
 
-    int arr[4][3] = {33, 44, 11, 55, 88, 22, 33, 66, 99, 11, 80, 9};
-    int(*p)[3] = arr;
-    printf("%d\n", p[2][1]);
-    printf("%d\n", *(*(p + 2) + 1));
-    printf("%d\n", sizeof(p));
-    printf("%d %d\n", sizeof(*p), sizeof(arr));
+    // int arr[4][3] = {33, 44, 11, 55, 88, 22, 33, 66, 99, 11, 80, 9};
+    // int(*p)[3] = arr;
+    // printf("%d\n", p[2][1]);
+    // printf("%d\n", *(*(p + 2) + 1));
+    // printf("%d\n", sizeof(p));
+    // printf("%d %d\n", sizeof(*p), sizeof(arr));
+
+    typedef int ag;
+    ag age = 10;
+    printf("%d\n", age);
 }
