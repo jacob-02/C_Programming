a8f0e94a2efcd9eae6ab7a4990e3cfe1a3683ec6
jackjack3006 <jvsanoj23@gmail.com>
2021-06-21 16:33:53 +0530

    Swapping values in main using a function

--- Testing/unti1.c
+++ Testing/unti1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main()
 {
-    int n = printf("%f", 200.00);
-
-    printf("The number of characters are %d\n", n);
+    int a = 100;
+    int b = 200;
+    printf("before call a is %d and b is %d\n", a, b);
+    // a is 100 and b is
+    swap(&a, &b);
+    printf("after call a is %d and b is %d\n", a, b);
 }
