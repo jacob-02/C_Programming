7c5d1a623102c4fbd2bc49a7ab6a99bef8f2e945
jackjack3006 <jvsanoj23@gmail.com>
2021-06-29 22:00:27 +0530

    Binary Search using Recusrion

--- Unit 4/binarySearch.c
+++ Unit 4/binarySearch.c
@@ -6,7 +6,8 @@
 // otherwise -1
 int binarySearch(int arr[], int l, int r, int x)
 {
-    if (r >= l) {
+    if (r >= l)
+    {
         int mid = l + (r - l) / 2;
 
         // If the element is present at the middle
@@ -31,7 +32,7 @@ int binarySearch(int arr[], int l, int r, int x)
 
 int main(void)
 {
-    int arr[] = { 2, 3, 4, 10, 40 };
+    int arr[] = {2, 3, 4, 10, 40};
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 10;
     int result = binarySearch(arr, 0, n - 1, x);
