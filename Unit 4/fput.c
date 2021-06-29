88058285673b8ffd2d6130c979dce59afb1b42fd
jackjack3006 <jvsanoj23@gmail.com>
2021-06-29 19:55:14 +0530

    Putting value from terminal into file

---
+++ Unit 4/fput.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    FILE *file;
+    char data[100];
+    file = fopen("test.txt", "w");
+
+    printf("Enter the data to be added into the file\n");
+    scanf("%s", data);
+
+    if (file == NULL)
+    {
+        printf("The file is not openable\n");
+    }
+
+    else
+    {
+        if (strlen(data) > 0)
+        {
+            fputs(data, file);
+            fputs("\n", file);
+        }
+
+        fclose(file);
+
+        printf("Data is succesfully written\nFile closed\n");
+        
+    }
+    
+}
