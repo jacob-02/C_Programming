1df7bc6e1a6c360f8824627058268021e3cee395
jackjack3006 <jvsanoj23@gmail.com>
2021-07-02 09:08:59 +0530

    Swaping using array of pointers

---
+++ Unit 4/arrayPointer1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct sample
+{
+    int a;
+    float b;
+};
+
+void display(struct sample* s[3], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d %f\n", s[i] -> a, s[i] -> b);
+    }
+    
+}
+
+void swap(struct sample** s1, struct sample** s2)
+{
+    struct sample *temp;
+    temp = *s1;
+    *s1 = *s2;
+    *s2 = temp;
+}
+
+int main()
+{
+    struct sample s1[] = {{22, 2.2}, {33, 3.3}, {44, 4.4}};
+    struct sample * s[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        s[i] = &s1[i];
+    }
+    printf("Before swap\n");
+    display(s, 3);
+    swap(&s[0], &s[2]);
+    printf("After swap\n");
+    display(s, 3);
+}
