bbad759acb6e8fa97bddd766005c4772aca9e35a
jackjack3006 <jvsanoj23@gmail.com>
2021-07-08 09:00:24 +0530

    Forceful boundaries

--- Unit 5/bitfield.c
+++ Unit 5/bitfield.c
@@ -7,10 +7,17 @@ struct sample
     // double c:3;  INVALID
 };
 
+struct sample1
+{
+    unsigned int a:5; // Each digit gives 2 values that can be added. 1 gives 0,1. 2 gives 0 to 3 and so on
+    unsigned int :0;
+    unsigned int y:8;
+};
 
 int main()
 {
     printf("Sizeof the structure is %lu\n", sizeof(struct sample));
+    printf("Sizeof the structure is %lu\n", sizeof(struct sample1));
     struct sample s;
 
     s.a = 5;    //Not allowed. large integer implicitly truncated to unsigned type 
