fe1cf584bdf8de714e61a1d80b6a57e42007c03f
jackjack3006 <jvsanoj23@gmail.com>
2021-07-08 08:43:55 +0530

    Common errors in bitfield

--- Unit 5/bitfield.c
+++ Unit 5/bitfield.c
@@ -2,8 +2,9 @@
 
 struct sample
 {
-    int a:1;
+    unsigned int a:1;
     int b:1;
+    // double c:3;  INVALID
 };
 
 
@@ -11,4 +12,7 @@ int main()
 {
     printf("Sizeof the structure is %lu\n", sizeof(struct sample));
     struct sample s;
+
+    s.a = 5;    //Not allowed. large integer implicitly truncated to unsigned type 
+    printf("%d\n", s.a);
 }
