4f9d4257b9b219f13465116728157ad4c83d0f5b
jackjack3006 <jvsanoj23@gmail.com>
2021-07-09 08:49:56 +0530

    Enums can hold only integers

--- Unit 5/moreEnum.c
+++ Unit 5/moreEnum.c
@@ -6,6 +6,11 @@ enum sample
     abc = INT_MAX
 };
 
+// enum examples
+// {
+//     // abc = 3.5, def = "hello"    // The enums can hold only integers
+// };
+
 int main()
 {
     printf("%d\n", abc);
