edfd76c10676539f518be08582461b7b57861b46
jackjack3006 <jvsanoj23@gmail.com>
2021-07-09 08:39:16 +0530

    Using enums. It is basically an integer. U can just upload multiple values for it

---
+++ Unit 5/enum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+enum months
+{
+    jan = 1, feb, mar = 3, april, may = 5, june, july, aug = 8, sep, oct, nov, dec = 12
+};
+
+int main()
+{
+    printf("The sizeof months is %ld\n", sizeof(enum months));
+    printf("%d %d %d %d %d\n", jan, feb, april, june, july);
+    printf("Enter the value for month\n");
+    enum months m;
+    scanf("%d", &m);
+    printf("%d\n", m);
+
+    enum months m1 = may;
+    printf("%d\n", m1);
+}
