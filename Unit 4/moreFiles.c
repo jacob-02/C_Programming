6cf61ed5b67070e3a3bdf081e9029ee7e2123882
jackjack3006 <jvsanoj23@gmail.com>
2021-07-13 09:29:44 +0530

    Writing from a file to a buffer to another file

--- Unit 4/moreFiles.c
+++ Unit 4/moreFiles.c
@@ -1,22 +1,47 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     FILE *fp = fopen("random.txt", "r");
+
+//     if (fp == NULL)
+//         printf("It cannot be opened\n");
+
+//     else
+//     {
+//         // printf("%p\n", fp);
+//         char ch;
+//         while ((ch = fgetc(fp)) != EOF)
+//         {
+//             fputc(ch, stdout);
+//             printf("%d\n", ftell(fp));
+//         }
+//         printf("\n");
+//         fclose(fp);
+//     }
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    FILE *fp = fopen("random.txt", "r");
+    FILE *fp1 = fopen("random.txt", "r");
+    FILE *fp2 = fopen("data.txt", "w");
 
-    if (fp == NULL)
-        printf("It cannot be opened\n");
+    char buffer[500];
+
+    if (fp1 == NULL || fp2 == NULL)
+    {
+        printf("The file has some issues\n");
+    }
 
     else
     {
-        // printf("%p\n", fp);
-        char ch;
-        while ((ch = fgetc(fp)) != EOF)
+        while (fgets(buffer, 500, fp1) != NULL)
         {
-            fputc(ch, stdout);
-            printf("%d\n", ftell(fp));
+            fputs(buffer, fp2);
         }
-        printf("\n");
-        fclose(fp);
+        fclose(fp1);
+        fclose(fp2);
     }
 }
