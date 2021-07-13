54339a7b56fda6c2adc0859b70442cef19da22e5
jackjack3006 <jvsanoj23@gmail.com>
2021-07-13 20:45:29 +0530

    Finding the environment that we are working on

---
+++ Unit 4/env.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+    int i;
+    for (int i = 0; envp[i] != NULL; i++)
+    {
+        printf("%d %s\n", i, envp[i]);
+    }
+    
+}
