ee5046073c558455cc607422f22932f6a0e5e7e1
jackjack3006 <jvsanoj23@gmail.com>
2021-07-13 19:18:44 +0530

    More preprocessorw

--- Unit 5/preprocessor.c
+++ Unit 5/preprocessor.c
@@ -1,9 +1,20 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("File Name : %s\n", __FILE__);
+//     printf("Date is : %s\n", __DATE__);
+//     printf("Time is : %s\n", __TIME__);
+//     printf("C Version is %ld\n", __STDC_VERSION__);
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    printf("File Name : %s\n", __FILE__);
-    printf("Date is : %s\n", __DATE__);
-    printf("Time is : %s\n", __TIME__);
-    printf("C Version is %ld\n", __STDC_VERSION__);
+    #ifdef MAX
+    printf("Hello\n");
+    #endif
+    printf("everyone\n");
+    return 0;
 }
