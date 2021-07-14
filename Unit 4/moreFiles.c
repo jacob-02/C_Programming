e2ce5c3c9001ad141d8e2c8a7df34e19f0394eb0
jackjack3006 <jvsanoj23@gmail.com>
2021-07-14 22:28:50 +0530

    Random jokes stuff xD

--- Unit 4/moreFiles.c
+++ Unit 4/moreFiles.c
@@ -1,25 +1,26 @@
-// // #include <stdio.h>
+#include <stdio.h>
 
-// // int main()
-// // {
-// //     FILE *fp = fopen("random.txt", "r");
+int main()
+{
+    FILE *fp = fopen("random.txt", "r");
+    FILE *fp1 = fopen("data.txt", "w");
+    
+    if (fp == NULL)
+        printf("It cannot be opened\n");
 
-// //     if (fp == NULL)
-// //         printf("It cannot be opened\n");
-
-// //     else
-// //     {
-// //         // printf("%p\n", fp);
-// //         char ch;
-// //         while ((ch = fgetc(fp)) != EOF)
-// //         {
-// //             fputc(ch, stdout);
-// //             printf("%d\n", ftell(fp));
-// //         }
-// //         printf("\n");
-// //         fclose(fp);
-// //     }
-// // }
+    else
+    {
+        // printf("%p\n", fp);
+        char ch;
+        while ((ch = fgetc(fp)) != EOF)
+        {
+            putc(ch, fp1);
+            // printf("%ld\n", ftell(fp));
+        }
+        printf("\n");
+        fclose(fp);
+    }
+}
 
 // #include <stdio.h>
 
@@ -46,40 +47,40 @@
 //     }
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    FILE *fp1 = fopen("random.txt", "r");
-    FILE *fp2 = fopen("data.txt", "w");
+// int main()
+// {
+//     FILE *fp1 = fopen("random.txt", "r");
+//     FILE *fp2 = fopen("data.txt", "w");
 
-    if (fp1 == NULL || fp2 == NULL)
-    {
-        printf("The file has some issues\n");
-    }
+//     if (fp1 == NULL || fp2 == NULL)
+//     {
+//         printf("The file has some issues\n");
+//     }
 
-    else
-    {
-        int a, temp = 0, count = 0;
-        printf("%ld\n", ftell(fp1));
-        fseek(fp1, 5, SEEK_SET);
-        printf("%ld\n", ftell(fp1));
+//     else
+//     {
+//         int a, temp = 0, count = 0;
+//         printf("%ld\n", ftell(fp1));
+//         fseek(fp1, 5, SEEK_SET);
+//         printf("%ld\n", ftell(fp1));
 
-        // fseek(fp1, 2, SEEK_SET);
-        // printf("%ld\n", ftell(fp1));
+//         // fseek(fp1, 2, SEEK_SET);
+//         // printf("%ld\n", ftell(fp1));
         
-        while (!feof(fp1))
-        {
-            fscanf(fp1, "%d", &a);
-            temp = temp + a;
-            fprintf(fp2, "%d    ", a);
-            count++;
-        }
-        fclose(fp1);
-        fclose(fp2);
-        printf("The sum of the %d numbers is %d\n", count, temp);
-    }
-}
+//         while (!feof(fp1))
+//         {
+//             fscanf(fp1, "%d", &a);
+//             temp = temp + a;
+//             fprintf(fp2, "%d    ", a);
+//             count++;
+//         }
+//         fclose(fp1);
+//         fclose(fp2);
+//         printf("The sum of the %d numbers is %d\n", count, temp);
+//     }
+// }
 
 // #include <stdio.h>
 
