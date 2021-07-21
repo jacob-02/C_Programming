d175bdd896327a3d862b53a090e8de964f76067a
jackjack3006 <jvsanoj23@gmail.com>
2021-07-21 15:01:17 +0530

    Brushing up bit fields

--- Unit 5/bitfield.c
+++ Unit 5/bitfield.c
@@ -2,14 +2,14 @@
 
 struct sample
 {
-    unsigned int a:3; // Each digit gives 2 values that can be added. 1 gives 0,1. 2 gives 0 to 3 and so on
+    int a:6; // Each digit gives 2 values that can be added. 1 gives 0,1. 2 gives 0 to 2 and so on
     int b:1;
     // double c:3;  INVALID
 };
 
 struct sample1
 {
-    unsigned int a:5; // Each digit gives 2 values that can be added. 1 gives 0,1. 2 gives 0 to 3 and so on
+    unsigned int a:5; // Each digit gives 2 values that can be added. 1 gives 0,1. 2 gives 0 to 2 and so on
     unsigned int :0;
     unsigned int y:8;
 };
@@ -20,6 +20,6 @@ int main()
     printf("Sizeof the structure is %lu\n", sizeof(struct sample1));
     struct sample s;
 
-    s.a = 5;    //Not allowed. large integer implicitly truncated to unsigned type 
+    s.a = 6;    //Not allowed. large integer implicitly truncated to unsigned type 
     printf("%d\n", s.a);
 }
