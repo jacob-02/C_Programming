0dec8bfa02176a2006967ddd305fbf684a0f9c67
jackjack3006 <jvsanoj23@gmail.com>
2021-07-21 15:35:02 +0530

    Using argvs. Modifying the code for a better understanding

--- Unit 5/commandLineArgs.c
+++ Unit 5/commandLineArgs.c
@@ -7,5 +7,6 @@ int main(int argc, char *argv[])
     printf("argv[0] : %s\n", argv[0]);   
     printf("argv[1] : %s\n", argv[1]);   
     printf("argv[2] : %s\n", argv[2]);   
-    printf("Sum of argv[1] and argv[2] : %d\n", atoi(argv[1]) + atoi(argv[2]));   
+    printf("argv[3] : %s\n", argv[3]);   
+    printf("Sum of argv[1] and argv[2] and argv[3] : %d\n", atoi(argv[1]) + atoi(argv[2]) + atoi(argv[3]));   
 }
