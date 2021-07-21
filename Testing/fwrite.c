d820118251be5e6c0104d2ac4f08bee7fce6b749
jackjack3006 <jvsanoj23@gmail.com>
2021-07-21 16:58:08 +0530

    Using fwrite detailed

---
+++ Testing/fwrite.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct student
+{
+    char name[10];
+    int roll;
+    float marks;
+};
+
+int main()
+{
+    FILE* p = fopen("abc.txt", "w");
+    struct student student_1 = {"Tina", 12, 88.123};
+    fwrite(&student_1, sizeof(student_1), 1, p);
+}
