ca97a8e01559b68ecbc9277f4b09795df8edac61
jackjack3006 <jvsanoj23@gmail.com>
2021-07-26 11:39:08 +0530

    Using #define

---
+++ Testing/Unit5/define.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define mul(x, y) x*y
+#define MAX 10
+#define MIN 5
+#define TEMP MAX+MIN
+
+int main()
+{
+    printf("%d\n", mul(MAX + 1, MIN - 1));
+    // #define MAX 20;
+    printf("%d\n", mul((MAX + 1), MIN - 1));
+    printf("%d\n", TEMP);
+}
