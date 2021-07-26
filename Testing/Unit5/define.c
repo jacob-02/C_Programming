6d0bae2beb4206875c3e08405ac7028960b37c40
jackjack3006 <jvsanoj23@gmail.com>
2021-07-26 11:39:29 +0530

    Using #define

--- Testing/Unit5/define.c
+++ Testing/Unit5/define.c
@@ -7,7 +7,7 @@
 int main()
 {
     printf("%d\n", mul(MAX + 1, MIN - 1));
-    // #define MAX 20;
+    // #define MAX 20; This is an error as the value of MAX is being redefined
     printf("%d\n", mul((MAX + 1), MIN - 1));
     printf("%d\n", TEMP);
 }
