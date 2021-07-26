beef7460d8eee551649e8bd9455ad680d62d59c2
jackjack3006 <jvsanoj23@gmail.com>
2021-07-26 11:29:10 +0530

    Using bitwise xor

---
+++ Testing/Unit2/function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int what(int a[], int n)
+{
+    int res = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        res = res ^ a[i];
+    }
+    return res;
+}
+
+int main()
+{
+    int a[] = {11, 44, 11, 33, 44};
+    printf("%d\n", what(a, 5));
+}
