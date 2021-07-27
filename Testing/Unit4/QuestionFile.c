b0cadaab19fdae2be56e87d352e8e54f3b144382
jackjack3006 <jvsanoj23@gmail.com>
2021-07-27 09:43:09 +0530

    ISA2 Question done using file redirection

--- Testing/Unit4/QuestionFile.c
+++ Testing/Unit4/QuestionFile.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main()
+int main(int argc, char *argv[])
 {
-    FILE *fp = fopen("data.txt", "r");
-    char line[50];
+    char line[500];
 
-    if (fp == NULL)
+    while (fgets(line, 50, stdin) != NULL)
     {
-        printf("The file is not found\n");
-    }
+        char *val = strtok(line, " ");
+        char *price = strtok(NULL, " ");
+        char *quantity = strtok(NULL, " ");
+        int id = atoi(val), cost = atoi(price), number = atoi(quantity);
 
-    else
-    {
-        while (fgets(line, 50, fp) != NULL)
+        if (cost < 10000)
         {
-            char *val = strtok(line, ",");
-            char *price = strtok(NULL, ",");
-            char *quantity = strtok(NULL, ",");
-            int id = atoi(val), cost = atoi(price), number = atoi(quantity);
-
-            if (cost < 10000)
-            {
-                printf("ID = %d\tTotal = %d\n", id, cost * number);
-            }
+            printf("ID = %d\tTotal = %d\n", id, cost * number);
         }
     }
-    fclose(fp);
 }
