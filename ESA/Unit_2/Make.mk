a25f31d7e08d1bae20ecc34b618562128729ee03
jackjack3006 <jvsanoj23@gmail.com>
2021-08-12 22:16:20 +0530

    Using gdb

--- ESA/Unit_2/Make.mk
+++ ESA/Unit_2/Make.mk
@@ -1,6 +1,6 @@
 a.out:palindrome.o server.o
 	gcc palindrome.o server.o
 palindrome.o:palindrome.c palindrome.h
-	gcc -c palindrome.c
+	gcc -g -o palindrome.c
 server.o:server.c palindrome.h
-	gcc -c server.c
+	gcc -g -o server.c
