a1594f65de97c0db27f130b2f1c714d6a5eb7cf7
jackjack3006 <jvsanoj23@gmail.com>
2021-08-13 18:54:23 +0530

    Learning about enums

--- Unit 5/enum.c
+++ Unit 5/enum.c
@@ -2,13 +2,13 @@
 
 enum months
 {
-    jan = 1, feb, mar = 3, april, may = 5, june, july, aug = 8, sep, oct, nov, dec = 12
+    jan = 1, feb, mar = 4, april, may = 5, june, july, aug = 8, sep, oct, nov, dec = 12
 };
 
 int main()
 {
     printf("The sizeof months is %ld\n", sizeof(enum months));
-    printf("%d %d %d %d %d\n", jan, feb, april, june, july);
+    printf("%d %d %d %d %d\n", jan, feb, april, may, july);
     printf("Enter the value for month\n");
     enum months m;
     scanf("%d", &m);
