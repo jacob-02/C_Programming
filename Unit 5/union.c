3b964ed4759d221673b0ef8f8741002336c3ec6c
jackjack3006 <jvsanoj23@gmail.com>
2021-08-13 16:53:03 +0530

    Using unions

--- Unit 5/union.c
+++ Unit 5/union.c
@@ -1,16 +1,29 @@
+// #include <stdio.h>
+// #include <string.h>
+// union car
+// {
+//     //Assumptions
+//     char name[10]; // 1 byte for char
+//     float price;   // 4 bytes for float
+// };
+// int main()
+// {
+//     union car c;
+//     // strcpy(c.name, "HELLO");
+//     // c.price = 200.43;
+//     printf("Memory size occupied by data in bytes : %d\n", sizeof(car));
+//     return 0;
+// }
+
 #include <stdio.h>
-#include <string.h>
 union car
-{
-    //Assumptions
+{                  //Assumptions
     char name[10]; // 1 byte for char
     float price;   // 4 bytes for float
 };
 int main()
 {
     union car c;
-    // strcpy(c.name, "HELLO");
-    c.price = 200.43;
-    printf("Memory size occupied by data in bytes : %ld\n", sizeof(c.price));
+    printf("Memory size occupied by data in bytes : %d\n", sizeof(car)); // Don't know why does this show error
     return 0;
 }
